7d7c837eb80832e10be92803cb2e6a1696e5d7d3
Shobhit <shobhit1256@gmail.com>
2024-06-27 00:01:56 +0530

    fix: path fix for copy final dist folder

--- deploy-service/src/aws.ts
+++ deploy-service/src/aws.ts
@@ -51,10 +51,10 @@ export async function downloadS3Folder(prefix: string) {
 
 
 export function copyFinalDist(id: string) {
-  const folderPath = path.join(__dirname, `output/${id}/build`);
+  const folderPath = path.join(__dirname, `output/${id}/dist`);
   const allFiles = getAllFiles(folderPath);
   allFiles.forEach(file => {
-      uploadFile(`build/${id}/` + file.slice(folderPath.length + 1), file);
+      uploadFile(`dist/${id}/` + file.slice(folderPath.length + 1), file);
   })
 }
 
