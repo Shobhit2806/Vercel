e8e0a460e31b5e7be079b2038874c5710e8d6b44
Shobhit <shobhit1256@gmail.com>
2024-06-27 15:00:08 +0530

    fix: final changes

--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -12,19 +12,17 @@ publisher.connect();
 const subscriber = createClient();
 subscriber.connect();
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 
 app.get("/status", async (req, res) => {
   const id = req.query.id;
   const response = await subscriber.hGet("status", id as string);
   res.json({
-      status: response
-  })
-})
+    status: response,
+  });
+});
 
 app.post("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl;
@@ -32,25 +30,20 @@ app.post("/deploy", async (req, res) => {
   await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
 
   const files = getAllFiles(path.join(__dirname, `output/${id}`));
-  
+
   // files.forEach(async (file) => {
   //   await uploadFile(file.slice(__dirname.length + 1), file);
   // });
 
   // @ts-ignore
-  const uploadFiles = async (files) => {
-    // @ts-ignore
-    const uploadPromises = files.map(async (file) => {
-      await uploadFile(file.slice(__dirname.length + 1), file);
-    });
-  
-    await Promise.all(uploadPromises);
-  };
-  uploadFiles(files);
+  files.forEach(async (file) => {
+    await uploadFile(file.slice(__dirname.length + 1), file);
+  });
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   publisher.lPush("build-queue", id);
   publisher.hSet("status", id, "uploaded");
   console.log(files);
-  
+
   res.json({
     id,
   });
